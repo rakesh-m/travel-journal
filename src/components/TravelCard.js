49002772a1ac33127c6c5fe1714a2808edb58caa
Rakesh Mhasawade <rakesh.mhasawade@hotmail.com>
2022-04-22 21:25:31 +0530

    Added card content and styling

--- src/components/TravelCard.js
+++ src/components/TravelCard.js
@@ -1,12 +1,31 @@
-// import images from './images'
+function importAll(r)
+{
+    let images = {};
+    r.keys().map((item, index) => { images[item.replace('./', '')] = r(item)})
+    return images;
+}
+
+const images = importAll(require.context('../images', false, /\.(png|jpg|svg)$/))
+
+console.log(JSON.stringify(images));
 
 export default function TravelCard(props)
 {
     return (
         <div className="travel--card">
-            <img src={`./images/${props.dest.photo}`} alt='Photo'/>
-            <div>{props.dest.country.toUpperCase()}</div>
-            <h1>{props.dest.title}</h1>
+            <img src={images[props.dest.photo]} alt={props.dest.photo}/>
+            {/* <img src={images} alt={props.dest.photo}/> */}
+            <div className="card--data">
+                <div className="card--country">
+                    {props.dest.country.toUpperCase()}
+                    <span><a href={props.dest.location} className="maps--text">View on Google Maps</a></span>
+                </div>
+                <h1>{props.dest.title}</h1>
+                <h3>{props.dest.start_date} - {props.dest.end_date}</h3>
+                <p className="card--content">
+                    {props.dest.content}
+                </p>
+            </div>
         </div>
     )
 }
--- src/travel-data.js
+++ src/travel-data.js
@@ -2,16 +2,28 @@ export default [
     {
         title: 'Ganges',
         photo: 'rishikesh.jpg',
-        country: 'India'
+        country: 'India',
+        start_date: '05 Oct, 2016',
+        end_date: '08 Oct, 2016',
+        location: 'https://goo.gl/maps/xUSKCnaRztFU7b4x5',
+        content: `The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings`
     },
     {
         title: 'Goa',
         photo: 'rishikesh.jpg',
-        country: 'India'
+        country: 'India',
+        start_date: '23 Jan, 2016',
+        end_date: '26 Jan, 2016',
+        location: 'https://goo.gl/maps/xUSKCnaRztFU7b4x5',
+        content: `The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings`
     },
     {
         country: 'India',
         photo: 'rishikesh.jpg',
-        title: 'Bangalore'
+        title: 'Bangalore',
+        start_date: '12 Oct, 2010',
+        end_date: '08 Dec, 2013',
+        location: 'https://goo.gl/maps/xUSKCnaRztFU7b4x5',
+        content: `The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings`
     }
 ]
